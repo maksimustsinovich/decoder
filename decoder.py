87e9d5b01206adf5e9bc45d751140ccdbda0fe86
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-06-13 18:05:43 +0300

    doc, refactor: some optimizations

--- decoder.py
+++ decoder.py
@@ -6,39 +6,44 @@ import argparse
 import struct
 
 
-def print_log(timestamp, time_offset_us, format_string, args, specifiers):
-    timestamp_string = "%010u.%06u" % (timestamp, time_offset_us)
+def print_log(time, time_offset, fmt_string, arguments, fmt_specifiers):
+    """Print log message with C-like string format"""
+
+    timestamp_string = "%010u.%06u" % (time, time_offset)
     arg_index = 0
-    specifier_index = 0
-    log_message = format_string
+    fmt_index = 0
+    log_message = fmt_string
 
     # ToDo: add format specifier length
 
-    while arg_index < len(args) and specifier_index < len(specifiers):
+    while arg_index < len(arguments) and fmt_index < len(fmt_specifiers):
         value = 0
 
-        if "X" in specifiers[specifier_index]:
-            value = hex(args[arg_index]).upper().removeprefix("0X")
+        current_specifier = fmt_specifiers[fmt_index]
+        current_argument = arguments[arg_index]
+        if "X" in current_specifier:
+            value = hex(current_argument).upper().removeprefix("0X")
 
-        if "x" in specifiers[specifier_index]:
-            value = hex(args[arg_index]).removeprefix("0x")
+        elif "x" in current_specifier:
+            value = hex(current_argument).removeprefix("0x")
 
-        if "lld" in specifiers[specifier_index] or "llu" in specifiers[specifier_index] or \
-                "u" in specifiers[specifier_index] or "d" in specifiers[specifier_index] or \
-                "s" in specifiers[specifier_index]:
-            value = str(args[arg_index])
+        elif ("lld" in current_specifier or "llu" in current_specifier or
+              "u" in current_specifier or "d" in current_specifier or "s" in current_specifier):
+            value = str(current_argument)
 
-        if "c" in specifiers[specifier_index]:
-            value = str(chr(args[arg_index]))
+        elif "c" in current_specifier:
+            value = str(chr(current_argument))
 
-        log_message = log_message.replace("%" + specifiers[specifier_index], value, 1)
+        log_message = log_message.replace("%" + current_specifier, value, 1)
         arg_index += 1
-        specifier_index += 1
+        fmt_index += 1
 
     print(timestamp_string, log_message)
 
 
 def calculate_checksum(byte_array):
+    """Calculates CRC8 checksum for byte array"""
+
     return int(crc8.crc8(byte_array).hexdigest(), 16)
 
 
@@ -47,7 +52,7 @@ if __name__ == "__main__":
     parser.add_argument('binary')
     parser.add_argument('-m', "--json", required=True)
 
-    args = parser.parse_args()
+    args = parser.parse_args()  # parse CLI arguments
 
     json_path = args.json
     binary_path = args.binary
@@ -65,10 +70,6 @@ if __name__ == "__main__":
             while offset < 512:
                 entry = page[offset:offset + 10]  # get fixed payload
 
-                # checksum = le_uint_from_bytes(entry[:1])  # 1 byte is real CRC8 checksum
-                # size = le_uint_from_bytes(entry[1:2])  # 2 byte is size
-                # string_addr = le_uint_from_bytes(entry[2:6])  # 3-6 bytes is string addr
-
                 checksum, size, string_addr, time_value = struct.unpack("<BBII", entry)
 
                 entry = page[offset:offset + size]  # get full payload
@@ -82,7 +83,6 @@ if __name__ == "__main__":
                     break
 
                 if string_addr == 0:  # SyncFrame string address is always 0
-                    # timestamp = le_uint_from_bytes(entry[6:10])  # 7-10 bytes of SyncFrame is UNIX time
                     timestamp = time_value
 
                     if size != 10:  # SyncFrame size is always 10
@@ -90,7 +90,6 @@ if __name__ == "__main__":
                         break
 
                 else:  # else is Message
-                    # time_offset_us = le_uint_from_bytes(entry[6:10])  # 7-10 bytes of Message is time offset
                     time_offset_us = time_value
 
                     format_string = data_format.get(string_addr)
@@ -108,33 +107,34 @@ if __name__ == "__main__":
                     data_index = 0
                     specifier_index = 0
                     while data_index < len(data):  # form args list by specifiers
-                        if "d" in specifiers[specifier_index]:
+                        specifier = specifiers[specifier_index]
+                        if "d" in specifier:
                             value = struct.unpack("<i", data[data_index:data_index + 4])
                             args.append(*value)
                             data_index += 4
 
-                        if "u" in specifiers[specifier_index] or "X" in specifiers[specifier_index] or \
-                                "x" in specifiers[specifier_index]:
+                        elif ("u" in specifier or "X" in specifier or
+                              "x" in specifier):
                             value = struct.unpack("<I", data[data_index:data_index + 4])
                             args.append(*value)
                             data_index += 4
 
-                        if "s" in specifiers[specifier_index]:
+                        elif "s" in specifier:
                             value = struct.unpack("<I", data[data_index:data_index + 4])
                             args.append(data_format.get(*value))
                             data_index += 4
 
-                        if "lld" in specifiers[specifier_index]:
+                        elif "lld" in specifier:
                             value = struct.unpack("<q", data[data_index:data_index + 8])
                             args.append(value)
                             data_index += 8
 
-                        if "llu" in specifiers[specifier_index]:
+                        elif "llu" in specifier:
                             value = struct.unpack("<Q", data[data_index:data_index + 8])
                             args.append(value)
                             data_index += 8
 
-                        if "c" in specifiers[specifier_index]:
+                        elif "c" in specifier:
                             args.append(data[data_index])
                             data_index += 1
 
